d3de223372b9a11eb120c6fb12f6a6b269d414e4
Awwab <mawwabkhank2006@gmail.com>
2024-02-29 21:27:05 +0500

    add two numbers

---
+++ addNumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x, y, sum;
+
+    cout << "Enter two numbers: " << endl;
+    cin >> x >> y;
+
+    sum = x + y;
+
+    cout << x << " + " << y << " = " << sum << endl;
+    
+    return 0;
+}
