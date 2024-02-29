ccd2841aaac78f6bf65ad0007cedc8dd66f3a5e5
Awwab <mawwabkhank2006@gmail.com>
2024-02-29 21:21:32 +0500

    print user input

---
+++ input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int number;
+    cout << "Enter any number: ";
+    cin >> number;
+
+    cout << "You entered " << number << " in the terminal" << endl;
+    return 0;
+}
