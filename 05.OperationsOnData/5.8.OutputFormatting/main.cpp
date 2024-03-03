f09fa79a716ab3aa4dd3dfd2053295e65ac86f86
Awwab <mawwabkhank2006@gmail.com>
2024-03-03 10:30:47 +0500

    output formatting -> show positive and negative sign

--- 05.OperationsOnData/5.8.OutputFormatting/main.cpp
+++ 05.OperationsOnData/5.8.OutputFormatting/main.cpp
@@ -165,5 +165,28 @@ int main(){
     //===================================================================
     std::cout << std::endl;
 
+    //showpos and noshowpos : show or hide the +  sign for positive numbers
+    
+    int pos_num {34};
+    int neg_num {-45};
+    
+    std::cout << "pos_num : " << pos_num << std::endl;
+    std::cout << "neg_num : " << neg_num << std::endl;
+    
+    std::cout << std::endl;
+    std::cout << std::showpos;
+    std::cout << "pos_num : " << pos_num << std::endl;
+    std::cout << "neg_num : " << neg_num << std::endl; 
+
+    std::cout << std::endl;
+    std::cout << std::noshowpos;
+    std::cout << "pos_num : " << pos_num << std::endl;
+    std::cout << "neg_num : " << neg_num << std::endl;   
+
+
+    //===================================================================
+    std::cout << std::endl;
+
+
     return 0;
 }
