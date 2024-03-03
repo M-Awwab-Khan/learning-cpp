0d1b14cc005ced0ecffda8204476edea3fb1f6d2
Awwab <mawwabkhank2006@gmail.com>
2024-03-03 10:34:05 +0500

    output formatting -> decimal point representation

--- 05.OperationsOnData/5.8.OutputFormatting/main.cpp
+++ 05.OperationsOnData/5.8.OutputFormatting/main.cpp
@@ -296,6 +296,28 @@ int main(){
     std::cout << std::endl;
 
 
+    //showpoint and noshowpoint : show trailing zeros if necessary
+    //Force output of the decimal point
     
+    double d {34.1};
+    double e {101.99};
+    double f {12.0};
+    int    g {45};
+    
+    std::cout << std::endl;
+    std::cout << "noshowpoint (default) : " << std::endl;
+    std::cout << "d : " << d << std::endl;
+    std::cout << "e : " << e << std::endl;
+    std::cout << "f : " << f << std::endl; // 12
+    std::cout << "g : " << g << std::endl;
+    
+    std::cout << std::endl;
+    std::cout << "showpoint: " << std::endl;
+    std::cout << std::showpoint;
+    std::cout << "d : " << d << std::endl;
+    std::cout << "e : " << e << std::endl;
+    std::cout << "f : " << f << std::endl; // 12.0
+    std::cout << "g : " << g << std::endl;
+
     return 0;
 }
