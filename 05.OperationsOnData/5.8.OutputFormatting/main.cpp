58082bf96118034d014665a46103a9b03b487dc2
Awwab <mawwabkhank2006@gmail.com>
2024-03-03 10:32:16 +0500

    output formatting -> uppercase and lowercase

--- 05.OperationsOnData/5.8.OutputFormatting/main.cpp
+++ 05.OperationsOnData/5.8.OutputFormatting/main.cpp
@@ -221,6 +221,27 @@ int main(){
     std::cout << std::endl;
 
 
+    //uppercase and nouppercase
+
+    pos_int = 717171;
+    
+    std::cout << "pos_int (nouppercase : default) : " << std::endl;
+    std::cout << "pos_int (dec) : " << std::dec << pos_int << std::endl;
+    std::cout << "pos_int (hex) : " << std::hex << pos_int << std::endl;
+    std::cout << "pos_int (oct) : " << std::oct << pos_int << std::endl;
+    
+    std::cout << std::endl;
+    std::cout << "pos_int (uppercase) : " << std::endl;
+    std::cout << std::uppercase;
+    std::cout << "pos_int (dec) : " << std::dec << pos_int << std::endl;
+    std::cout << "pos_int (hex) : " << std::hex << pos_int << std::endl;
+    std::cout << "pos_int (oct) : " << std::oct << pos_int << std::endl;
+    
+    
+    //===================================================================
+    std::cout << std::endl;
+
+
     
 
     return 0;
