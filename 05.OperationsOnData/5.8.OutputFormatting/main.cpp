c2bdf1bf8b98becfca9195767f0f021505d1ec7a
Awwab <mawwabkhank2006@gmail.com>
2024-03-03 10:30:02 +0500

    output formatting -> boolalpha

--- 05.OperationsOnData/5.8.OutputFormatting/main.cpp
+++ 05.OperationsOnData/5.8.OutputFormatting/main.cpp
@@ -144,6 +144,26 @@ int main(){
     //===================================================================
     std::cout << std::endl;
 
+    //boolalpha and noboolapha : control bool output format : 1/0 or true/false
+    
+    bool condition {true};
+    bool other_condition {false};
+    
+    std::cout << "condition : " << condition << std::endl;
+    std::cout << "other_condition : " << other_condition << std::endl;
+    
+    std::cout << std::endl;
+    std::cout << std::boolalpha;
+    std::cout << "condition : " << condition << std::endl;
+    std::cout << "other_condition : " << other_condition << std::endl;
+    
+    std::cout << std::endl;
+    std::cout << std::noboolalpha;
+    std::cout << "condition : " << condition << std::endl;
+    std::cout << "other_condition : " << other_condition << std::endl;
+    
+    //===================================================================
+    std::cout << std::endl;
 
     return 0;
 }
