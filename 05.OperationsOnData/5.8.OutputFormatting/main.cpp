40c36ac4c55224a7f7a0aad8d65b829b1d7ea447
Awwab <mawwabkhank2006@gmail.com>
2024-03-03 10:33:40 +0500

    output formatting -> set precision

--- 05.OperationsOnData/5.8.OutputFormatting/main.cpp
+++ 05.OperationsOnData/5.8.OutputFormatting/main.cpp
@@ -279,7 +279,23 @@ int main(){
     //===================================================================
     std::cout << std::endl;
 
+    //setprecision() : the number of digits printed out for a floating point. Default is 6
     
+    a = 3.1415926535897932384626433832795;
+    
+    std::cout << std::endl;
+    std::cout << "a (default precision(6)) : " << a <<  std::endl;
+    std::cout << std::setprecision(10);
+    std::cout << "a (precision(10)) : " << a << std::endl;
+    std::cout << std::setprecision(20);
+    std::cout << "a (precision(20)) : " << a << std::endl;
+    
+    //If the precision is bigger than supported by the type, you'll just print garbage.
+    
+    //===================================================================
+    std::cout << std::endl;
 
+
+    
     return 0;
 }
