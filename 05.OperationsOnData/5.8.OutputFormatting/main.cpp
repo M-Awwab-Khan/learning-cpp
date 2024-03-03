21d9cee6853791bf6d35f26bcb803a708d544b7f
Awwab <mawwabkhank2006@gmail.com>
2024-03-03 10:33:11 +0500

    output formatting -> scientific and fixed notation

--- 05.OperationsOnData/5.8.OutputFormatting/main.cpp
+++ 05.OperationsOnData/5.8.OutputFormatting/main.cpp
@@ -242,6 +242,43 @@ int main(){
     std::cout << std::endl;
 
 
+    //fixed and scientific : for floating point values
+    
+    double a{ 3.1415926535897932384626433832795 };
+    double b{ 2006.0 };
+    double c{ 1.34e-10 };
+    
+    std::cout << std::endl;
+    std::cout << "double values (default : use scientific where necessary) : " << std::endl;
+    std::cout << "a : " << a << std::endl;
+    std::cout << "b : " << b << std::endl;
+    std::cout << "c : " << c << std::endl;
+    
+    std::cout << std::endl;
+    std::cout << "double values (fixed) : " << std::endl;
+    std::cout << std::fixed;
+    std::cout << "a : " << a << std::endl;
+    std::cout << "b : " << b << std::endl;
+    std::cout << "c : " << c << std::endl;
+    
+    std::cout << std::endl;
+    std::cout << "double values (scientific) : " << std::endl;
+    std::cout << std::scientific;
+    std::cout << "a : " << a << std::endl;
+    std::cout << "b : " << b << std::endl;
+    std::cout << "c : " << c << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "double values (back to defaults) : " << std::endl;
+    std::cout.unsetf(std::ios::scientific | std::ios::fixed); // Hack
+    std::cout << "a : " << a << std::endl;
+    std::cout << "b : " << b << std::endl;
+    std::cout << "c : " << c << std::endl;
+    
+    
+    //===================================================================
+    std::cout << std::endl;
+
     
 
     return 0;
