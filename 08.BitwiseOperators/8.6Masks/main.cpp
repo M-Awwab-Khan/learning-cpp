7b3c0b0c25dc6ad050c324fdb9ac4cf4b01961a3
Awwab <mawwabkhank2006@gmail.com>
2024-03-05 19:34:43 +0500

    masking -> using xor ^

--- 08.BitwiseOperators/8.6Masks/main.cpp
+++ 08.BitwiseOperators/8.6Masks/main.cpp
@@ -94,6 +94,27 @@ int main(){
     std::cout << "bit7 is " << static_cast<bool>(var & mask_bit_7) << std::endl;
 
 
+	//Toggle bits
+	//Toggle : var ^ mask
 	
+	//Toggle bit 0
+	std::cout << std::endl;
+	std::cout << "Toggle bit 0" << std::endl;
+    var ^= mask_bit_0;
+	std::cout << std::setw(COLUMN_WIDTH) <<  "var : "
+		 << std::setw(COLUMN_WIDTH) << std::bitset<8>(var) << std::endl;
+
+    //Toggle bit7 
+	std::cout << "Toggle bit 7" << std::endl;
+    var ^= mask_bit_7;
+	std::cout << std::setw(COLUMN_WIDTH) <<  "var : "
+		 << std::setw(COLUMN_WIDTH) << std::bitset<8>(var) << std::endl;
+
+	//Toggle multiple bits in one go : the 4 higher bits
+	std::cout << "Toggle multiple bits in one go : the 4 higher bits" << std::endl;
+    var ^= (mask_bit_7 | mask_bit_6 | mask_bit_5 | mask_bit_4);
+	std::cout << std::setw(COLUMN_WIDTH) <<  "var : "
+		 << std::setw(COLUMN_WIDTH) << std::bitset<8>(var) << std::endl;
+   
     return 0;
 }
