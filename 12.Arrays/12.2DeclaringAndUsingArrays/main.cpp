240fa90630f88c3d9dbb4185f98c330084b5afd3
Awwab <mawwabkhank2006@gmail.com>
2024-03-09 22:34:37 +0500

    various array initialization techniques

--- 12.Arrays/12.2DeclaringAndUsingArrays/main.cpp
+++ 12.Arrays/12.2DeclaringAndUsingArrays/main.cpp
@@ -5,7 +5,7 @@
 int main(){
 
     //Declare an array of ints
-   int scores [10]; // Junk data
+   // int scores [10]; // Junk data
 
     //Read data
 
@@ -35,6 +35,7 @@ int main(){
 
 
    //Write data in a loop
+   /*
    for(size_t i{0}; i < 10 ; ++i){
        scores[i] = i * 10;
    }
@@ -43,8 +44,33 @@ int main(){
     for( size_t i {0} ; i < 10 ; ++i){
         std::cout << "scores [" << i << "] : " << scores[i] << std::endl;
     }
+    */
 
+    //Declare and initialize at the same time
+	double salaries[5] {12.7, 7.5, 13.2, 8.1, 9.3};
+	
+	for(size_t i{0}; i < 5; ++i){
+		std::cout << "salary[" << i << "] : " << salaries[i] << std::endl;
+	}
+  
 
-   
+    //If you don't initialize all the elements, those you leave out
+    //are initialized to 0
+
+    int families[5] {12, 7, 5};
+	
+	for(size_t i{0}; i < 5; ++i){
+		std::cout << "families[" << i << "] : " << families[i] << std::endl;
+	}
+    
+
+   	//Omit the size of the array at declaration
+    int class_sizes[] {10,12,15,11,18,17,23,56}; 
+	
+	// Will print this with a range based for loop
+	for(auto value : class_sizes){
+		std::cout << "value : " << value << std::endl;
+	}
+    
     return 0;
 }
