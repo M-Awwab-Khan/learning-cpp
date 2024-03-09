11afaa57be4641624af40ffe7a1a9d00fa2c874d
Awwab <mawwabkhank2006@gmail.com>
2024-03-09 22:35:23 +0500

    read only arrays

--- 12.Arrays/12.2DeclaringAndUsingArrays/main.cpp
+++ 12.Arrays/12.2DeclaringAndUsingArrays/main.cpp
@@ -47,30 +47,42 @@ int main(){
     */
 
     //Declare and initialize at the same time
+    /*
 	double salaries[5] {12.7, 7.5, 13.2, 8.1, 9.3};
 	
 	for(size_t i{0}; i < 5; ++i){
 		std::cout << "salary[" << i << "] : " << salaries[i] << std::endl;
 	}
+    */
   
 
     //If you don't initialize all the elements, those you leave out
     //are initialized to 0
-
+    /*
     int families[5] {12, 7, 5};
 	
 	for(size_t i{0}; i < 5; ++i){
 		std::cout << "families[" << i << "] : " << families[i] << std::endl;
 	}
+    */
     
 
    	//Omit the size of the array at declaration
+   /*
     int class_sizes[] {10,12,15,11,18,17,23,56}; 
 	
 	// Will print this with a range based for loop
 	for(auto value : class_sizes){
 		std::cout << "value : " << value << std::endl;
 	}
+    */
     
+
+
+   // Read only arrays
+
+   const int birds[] {10,12,15,11,18,17,23,56}; 
+   birds[2] = 8;
+ 
     return 0;
 }
