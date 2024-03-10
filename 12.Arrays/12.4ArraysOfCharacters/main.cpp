4e3ab1aff37c323d5acd2762a23a4b3bd6698c66
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-10 14:00:25 +0500

    change characters in array

--- 12.Arrays/12.4ArraysOfCharacters/main.cpp
+++ 12.Arrays/12.4ArraysOfCharacters/main.cpp
@@ -18,5 +18,27 @@ int main(){
     }
     std::cout << std::endl;
     */
+
+    
+   /*
+    //Change characters in our array
+    message[1] = 'a';
+
+    //Print out the array through looping
+    std::cout << "message : ";
+    for( auto c : message){
+        std::cout << c ;
+    }
+    std::cout << std::endl;
+    */
+
+   
+   
+    // Will probably print garbage after your char array
+    /*
+    std::cout << "message : " << message << std::endl;
+    */
+   
+
     return 0;
 }
