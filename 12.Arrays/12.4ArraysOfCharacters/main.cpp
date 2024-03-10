664c8cce1ad03b8aa330ffbe864f6c21ff340599
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-10 13:58:39 +0500

    character arrays -> formation and printing

---
+++ 12.Arrays/12.4ArraysOfCharacters/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main(){
+
+	//Declare array
+    char message [5]  {"Hello"};
+    //int data[5] {1,2,3,3,3};
+
+    //std::cout << "data : " << data << std::endl;
+    
+  
+    //Print out the array through looping
+    /*
+    std::cout << "message : ";
+    for( auto c : message){
+        std::cout << c ;
+    }
+    std::cout << std::endl;
+    */
+    return 0;
+}
