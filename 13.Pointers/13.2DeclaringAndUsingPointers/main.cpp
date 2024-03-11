91ab5238e7195ae187cdcc52f0b82c1166d7b7e8
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-11 21:57:24 +0500

    initialize and size of pointers

---
+++ 13.Pointers/13.2DeclaringAndUsingPointers/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+
+
+int main(){
+    /*
+    //Declare and initialize pointer
+    int* p_number {}; // Will initialize with nullptr
+    double*  p_fractional_number{};
+
+    //Explicitly initialize with nullptr
+    int* p_number1{nullptr};
+    int* p_fractional_number1{nullptr};
+
+    //Pointers to different variables are  of the same size
+    std::cout << "sizeof(int) : " << sizeof(int) << std::endl; // 4
+    std::cout << "sizeof(double) : " << sizeof(double) << std::endl; // 8
+    std::cout << "sizeof(double*) : " << sizeof(double*) << std::endl;
+    std::cout << "sizeof(int*) : " << sizeof(int*) << std::endl;
+    std::cout << "sizeof(p_number1) : " << sizeof(p_number1) << std::endl;
+    std::cout << "sizeof(p_fractional_number1) : " << sizeof(p_fractional_number1) << std::endl;
+
+
+    //It doesn't matter if you put the * close to data type or to varible name
+    int*  p_number2{nullptr};
+    int * p_number3{nullptr};
+    int  *p_number4{nullptr};
+
+    int *p_number5{}, other_int_var{};
+    int* p_number6{}, other_int_var6{};	// Confusing as you wonder if other_int_var6
+										//is also a pointer to int. It is not
+										// The structure is exactly the same for the
+										//previous statement
+
+    std::cout << "sizeof(p_number5) : " << sizeof(p_number5) << std::endl;
+    std::cout << "sizeof(other_int_var) : " << sizeof(other_int_var) << std::endl;
+    std::cout << "sizeof(p_number6) : " << sizeof(p_number6) << std::endl;
+    std::cout << "sizeof(other_int_var6) : " << sizeof(other_int_var6) << std::endl;
+
+    //It is better to separate these declarations on different lines though
+    int *p_number7{};
+    int other_int_var7{}; // No room for confusion this time
+    */
+
+
+
+    return 0;
+}
