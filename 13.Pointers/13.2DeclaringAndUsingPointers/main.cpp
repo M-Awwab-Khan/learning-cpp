b86ebb98c13a756c87f0ac810d317c9ed6810cf5
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-11 21:58:22 +0500

    assigning address to pointers

--- 13.Pointers/13.2DeclaringAndUsingPointers/main.cpp
+++ 13.Pointers/13.2DeclaringAndUsingPointers/main.cpp
@@ -43,6 +43,27 @@ int main(){
     */
 
 
+    //Initializing pointers and assigning them data
+    //We know that pointers store addresses of variables
+    int int_var {43};
+    int *p_int{&int_var};// The address of operator (&);
+    
+    std::cout << "Int var : " << int_var << std::endl;
+    std::cout << "p_int (Address in memory)  : " << p_int << std::endl;
+
+    //You can also change the address stored in a pointer any time
+    int int_var1 {65};
+    p_int = &int_var1; // Assign a different address to the pointer
+    std::cout << "p_int (with different address)  : " << p_int << std::endl;
+
+
+    //Can't cross assign between pointers of different types
+    int *p_int1{nullptr};
+    double double_var{33};
+
+    //p_int = &double_var; // Compiler error
+
+
 
     return 0;
 }
