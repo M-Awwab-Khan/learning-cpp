5519ff2917cbef96f8690a95c4bc87bf54261a69
Awwab <mawwabkhank2006@gmail.com>
2024-03-15 11:12:36 +0500

    dynamic arrays initialization and deletion

---
+++ 13.Pointers/13.20DynamicallyAllocatedArrays/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main(){
+
+    const size_t size{10};
+    
+    //Different ways you can declare an array
+	//dynamically and how they are initialized
+	
+    double *p_salaries { new double[size]}; // salaries array will
+															//contain garbage  values
+    int *p_students { new(std::nothrow) int[size]{} }; // All values initialized to 0 
+    
+    double *p_scores { new(std::nothrow) double[size]{1,2,3,4,5}}; // Allocating memory space
+																	// for an array  of size double
+																	//vars. First 5 will be initialized
+																	//with 1,2,3,4,5, and the 
+																	//rest will be 0's.
+
+
+    //nullptr check and use the allocated array
+    if(p_scores){
+        std::cout << "size of scores (it's a regular pointer) : " << sizeof(p_scores) << std::endl;
+        std::cout << "Successfully allocated memory for scores."<< std::endl;
+        
+        //Print out elements. Can use regular array access notation, or pointer arithmetic
+        for( size_t i{}; i < size ; ++i){
+           std::cout << "value : " << p_scores[i] << " : " << *(p_scores + i) << std::endl; 
+        }
+    }
+
+    delete [] p_salaries;
+    p_salaries = nullptr;
+
+    delete [] p_students;
+    p_students = nullptr;
+
+   delete [] p_scores;
+   p_scores = nullptr;
+
+    return 0;
+}
