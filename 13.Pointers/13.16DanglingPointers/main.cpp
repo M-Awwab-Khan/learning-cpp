ab2556572d768823c6ec3ae4a3430be2b511c158
Awwab <mawwabkhank2006@gmail.com>
2024-03-15 11:06:12 +0500

    dangling pointer -> 3 -> Multiple pointer pointing to same address

--- 13.Pointers/13.16DanglingPointers/main.cpp
+++ 13.Pointers/13.16DanglingPointers/main.cpp
@@ -16,7 +16,7 @@ int main(){
 
 
    //Case 2 : deleted pointer
-   
+   /*
    std::cout << std::endl;
    std::cout << "Case 2 : Deleted pointer" << std::endl;
    int * p_number1 {new int{67}};
@@ -26,7 +26,28 @@ int main(){
    delete p_number1;
 		
    std::cout << "*p_number1(after delete) : " << *p_number1 << std::endl;
-  
+   */
+
+
+	//Case 3 : Multiple pointers pointing to same address
+    
+	std::cout << std::endl;
+	std::cout << "Case 3 : Multiple pointers pointing to same address : " << std::endl;
+	
+    int *p_number3 {new int{83}};
+    int *p_number4 {p_number3};
+    
+    std::cout << "p_number3 - " << p_number3 << " - " << *p_number3 << std::endl;
+    std::cout << "p_number4 - " << p_number4 << " - " << *p_number4 << std::endl;
+    
+    //Deleting p_number3
+    delete p_number3;
+    
+    //p_number4 points to deleted memory. Dereferncing it will lead to
+	//undefined behaviour : Crash/ garbage or whatever
+    std::cout<< "p_number4(after deleting p_number3) - " << p_number4 << " - " << *p_number4 << std::endl;
+   
+
 
     std::cout << "Program is ending well" << std::endl;
   
