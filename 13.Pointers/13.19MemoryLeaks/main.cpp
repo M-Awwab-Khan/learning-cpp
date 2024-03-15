87997b532df56fa4bc26ece65d5a72cb246ec8da
Awwab <mawwabkhank2006@gmail.com>
2024-03-15 11:10:42 +0500

    memory leaks -> Case 2

--- 13.Pointers/13.19MemoryLeaks/main.cpp
+++ 13.Pointers/13.19MemoryLeaks/main.cpp
@@ -2,7 +2,7 @@
 
 int main(){
 
-    
+    /*
     int *p_number {new int{67}}; // Points to some address, let's call that address1
     
     //Should delete and reset here 
@@ -12,8 +12,21 @@ int main(){
     p_number = &number; // Now p_number points to address2 , but address1 is still in use by 
                         // our program. But our program has lost access to that memory location.
 						//Memory has been leaked.
-   
+    */
 
+	//Double allocation 
+    
+    int *p_number1 {new int{55}};
+    
+	//Use the pointer
+    
+	//Should delete and reset here.
+	
+    p_number1 = new int{44}; // memory with int{55} leaked.
+
+    delete p_number1;
+    p_number1 = nullptr;
+   
     std::cout << "Program ending well" << std::endl;
     return 0;
 }
