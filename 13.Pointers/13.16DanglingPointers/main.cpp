ca1a00e141fa2d4602cb615853a21a1828990007
Awwab <mawwabkhank2006@gmail.com>
2024-03-15 11:05:25 +0500

    dangling pointer -> 2 -> deleted pointer

--- 13.Pointers/13.16DanglingPointers/main.cpp
+++ 13.Pointers/13.16DanglingPointers/main.cpp
@@ -5,14 +5,28 @@
 int main(){
 
 	//Case1 : Uninitialized pointer
-    
+    /*
 	int * p_number; // Dangling uninitialized pointer
    
 	std::cout << std::endl;
 	std::cout << "Case 1 : Uninitialized pointer : ." << std::endl;
 	std::cout << "p_number : " << p_number << std::endl;
     std::cout << "*p_number : " << *p_number << std::endl; //CRASH!
+    */
+
+
+   //Case 2 : deleted pointer
    
+   std::cout << std::endl;
+   std::cout << "Case 2 : Deleted pointer" << std::endl;
+   int * p_number1 {new int{67}};
+   
+   std::cout << "*p_number1 (before delete) : " << *p_number1 << std::endl;
+   
+   delete p_number1;
+		
+   std::cout << "*p_number1(after delete) : " << *p_number1 << std::endl;
+  
 
     std::cout << "Program is ending well" << std::endl;
   
