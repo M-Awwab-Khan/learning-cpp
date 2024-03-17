862691269d4693faf0d7396812e31ce9590df95c
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-17 20:30:35 +0500

    format properly

--- 15.CharacterManipulationAndStrings/15.6DeclaringAndUsingStdString/main.cpp
+++ 15.CharacterManipulationAndStrings/15.6DeclaringAndUsingStdString/main.cpp
@@ -5,14 +5,14 @@ int main(){
 
 
 	std::string full_name;//Empty string
-    std::string planet {"Earth. Where the sky is blue"};//Initialize with string literal
+  std::string planet {"Earth. Where the sky is blue"};//Initialize with string literal
 	std::string prefered_planet{planet};//Initialize with other existing string
-    std::string message {"Hello there",5};	//Initialize with part of a string literal.
+  std::string message {"Hello there",5};	//Initialize with part of a string literal.
 											//Contains hello
-    std::string weird_message(4,'e');//Initialize with multiple copies of a char
+  std::string weird_message(4,'e');//Initialize with multiple copies of a char
 									 // contains eeee
-    std::string greeting{"Hello World"};
-    std::string saying_hello{ greeting,6,5};//Initialize with part of an existing std::string
+  std::string greeting{"Hello World"};
+  std::string saying_hello{ greeting,6,5};//Initialize with part of an existing std::string
 											// starting at index 6, taking 5 characters.
 											//Will contain World.
 	
