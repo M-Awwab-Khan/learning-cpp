a10f4f08c45eebb42d0b48359f402a57ea69f66e
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-17 20:37:23 +0500

    workarounds for string literal concatenation

--- 15.CharacterManipulationAndStrings/15.7ConcatenatingStdStrings/main.cpp
+++ 15.CharacterManipulationAndStrings/15.7ConcatenatingStdStrings/main.cpp
@@ -27,6 +27,36 @@ int main(){
 	//std::string str4 = "Hello" + "World";  // Compiler Error
     */
 
+  //A few ways around string literal concatenation
+    /*
+	std::cout << std::endl;
+	std::cout << "A few ways around string literal concatenation" << std::endl;
+            
+	std::string str5 {"Hello World"}; 	// Make the string one literal
+										//in the first place
+	std::string str6 {"Hello" " World"};// Put the literals side by side
+										//without the + in between
+	std::string str7{std::string{"Hello"} + " World"};//Turn one or both into a std::string
+													 //object  and do the concatenation
+	std::cout << "str5 : " << str5 << std::endl;
+	std::cout << "str6 : " << str6 << std::endl;
+	std::cout << "str7 : " << str7 << std::endl;
+                                                     
+	using namespace std::string_literals; // necessary for the s suffix
+										  // This polutes the namespace in main
+	std::string str8 {"Hello"s + " World"};// Turn one or both into strings using the 
+                                    //s suffix . the string_literals namespace has to be imported
+                                    //for this to work
+	std::cout << "str8 : " << str8 << std::endl;
+									
+	//A better way to not polute the namespace
+	std::string str9;
+	{
+		using namespace std::string_literals;
+		str9 = "Hello"s + " World";
+	}
+	std::cout << "str9 : " << str9 << std::endl;
+    */
   return 0;
 
 }
