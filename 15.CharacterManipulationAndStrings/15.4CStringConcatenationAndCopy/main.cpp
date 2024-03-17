fb5041d1a68ce70c05f57adad09c8eedc51eb21f
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-17 20:03:48 +0500

    cstring copy facility

--- 15.CharacterManipulationAndStrings/15.4CStringConcatenationAndCopy/main.cpp
+++ 15.CharacterManipulationAndStrings/15.4CStringConcatenationAndCopy/main.cpp
@@ -43,5 +43,57 @@ int main(){
     */
 
 
+    //std::strncat  : concatenates n characters from src to dest and
+	//returns a pointer to the result string
+    //signature : char *strncat( char *dest, const char *src, std::size_t count );
+    /*
+    std::cout << std::endl;
+    std::cout << "std::strncat:" << std::endl;
+    char dest2[50] { "Hello" };
+    
+    char source2[30] = {" There is a bird on my window"};
+    
+    //You can even use the returned pointer immediately for print out
+    //This is a pattern you'll see a lot in C++ code out there.
+    std::cout << std::strncat(dest2,source2,6) << std::endl;
+    
+    //Or you can do std::strncat separately and print dest2
+    std::strncat(dest2,source2,6);
+    std::cout << "The concatenated string is : " << dest2 << std::endl;
+    */
+
+
+    //std::strcpy - signature : char* strcpy( char* dest, const char* src );
+    /*
+    std::cout << std::endl;
+	std::cout << "std::strcpy : " << std::endl;
+    const char* source3 = "C++ is a multipurpose programming language.";
+    char *dest3 = new char[std::strlen(source3) + 1];// +1 for the null character
+													//Contains garbage data
+													//Not initialized
+													
+    std::strcpy(dest3,source3);
+    
+    std::cout << "sizeof(dest3) : " << sizeof(dest3) << std::endl;
+    std::cout << "std::strlen(dest3) : " << std::strlen(dest3) << std::endl;
+    std::cout << "dest3 : " << dest3 << std::endl;
+    */
+
+
+    //std::strncpy : Copy n characters from src to dest -
+    //signature : char *strncpy( char *dest, const char *src, std::size_t count );
+	std::cout << std::endl;
+    std::cout << "std::strncpy:" << std::endl;
+    const char* source4 = "Hello";
+    char dest4[] = {'a', 'b', 'c', 'd', 'e', 'f','\0'}; // Have to put the terminating
+														//null char if we want to print
+    
+    std::cout << "dest4 : " << dest4 << std::endl;
+    
+	std::cout << "Copying..." << std::endl;
+    std::strncpy(dest4,source4,5);
+    
+    std::cout << "dest4 : " << dest4 << std::endl;
+
     return 0;
 }
