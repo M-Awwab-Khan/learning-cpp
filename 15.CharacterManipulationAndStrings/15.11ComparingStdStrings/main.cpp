81d0cecb3ca8fd8d55d26c3c90ac4ca525dbd06c
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-19 16:33:28 +0500

    comparing std::strings (lexicographically)

---
+++ 15.CharacterManipulationAndStrings/15.11ComparingStdStrings/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+
+int main(){
+
+	std::string hello_str{"Hello"};
+    std::string bello_str{"Bello"};
+    
+    /*
+	//Using the comparison operators
+	std::cout << std::endl;
+	std::cout << "Comparing with comparison operators : " << std::endl;
+	
+	std::cout << std::boolalpha;
+    std::cout << hello_str << "==" <<  bello_str <<" : " << (hello_str == bello_str) << std::endl;//false
+    std::cout << hello_str << "!=" <<  bello_str <<" : " << (hello_str != bello_str) << std::endl; //true
+    std::cout << hello_str << ">" <<  bello_str <<" : " << (hello_str > bello_str) << std::endl; // true
+    std::cout << hello_str << ">=" <<  bello_str <<" : " << (hello_str >= bello_str) << std::endl; // true
+    std::cout << hello_str << "<" <<  bello_str <<" : " << (hello_str < bello_str) << std::endl; // false
+    std::cout << hello_str << "<=" <<  bello_str <<" : " << (hello_str <= bello_str) << std::endl; // false
+
+    */
+    return 0;
+}
