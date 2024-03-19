6f7b74b19ada91bdbd978cec35965bb315f69895
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-19 16:49:01 +0500

    replacing substrings in std::string

---
+++ 15.CharacterManipulationAndStrings/15.13StdStringReplacingCopyingResizingSwapping/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+int main(){
+
+	//Replacing (1)
+    //basic_string& replace( size_type pos, size_type count,const basic_string& str );
+    //Replace the range [pos, pos + count] with str
+	/*
+	std::string str1 {"Finding Nemo"}; // Replace Finding with 'Searching For'
+	std::string str1_2 {"Searcing For"};
+	std::cout << "str1 : " << str1 << std::endl;
+	
+	std::cout << "Replacing..." << std::endl;
+    str1.replace(0,7,str1_2);
+	
+    std::cout << "str1 : " << str1 << std::endl;
+    */
+
+   	//Replacing - (2)
+    //basic_string& replace( size_type pos, size_type count,
+                       //const basic_string& str,
+                       //size_type pos2, size_type count2 = npos );
+    /*                   
+    //Replace in part of str, not its entirety
+	std::string str2 {"Finding Nemo"};
+    std::string str3 {"The Horse was Found in the Fields Searching For Food"};
+	
+	std::cout << "str2 : " << str2 << std::endl;
+	
+	std::cout << "Replacing..." << std::endl;
+    str2.replace(0,7,str3,34,13);
+	
+    std::cout << "str2 : " << str2 << std::endl;
+    */
+
+   
+    return 0;
+}
