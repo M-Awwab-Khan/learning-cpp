1f927dad6db808a0f7b89d684f9c472036de28f4
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-19 16:49:33 +0500

    copying one string to another

--- 15.CharacterManipulationAndStrings/15.13StdStringReplacingCopyingResizingSwapping/main.cpp
+++ 15.CharacterManipulationAndStrings/15.13StdStringReplacingCopyingResizingSwapping/main.cpp
@@ -34,6 +34,23 @@ int main(){
     std::cout << "str2 : " << str2 << std::endl;
     */
 
-   
+    //Copying
+    //size_type copy( CharT* dest,size_type count,size_type pos = 0)
+	//Copy a subsection of *this string to dest. Dest is pre-allocated
+	// The resulting character string is not null-terminated. Be careful
+    /*
+    std::string str4{"Climbing Kirimanjaro"};
+    char txt4[15] {}; //Initialized with zero equivalent for characters which is '\0'
+    std::cout << "std::size(txt4) : " << std::size(txt4) << std::endl;
+    std::cout << "txt4 : " << txt4 << std::endl;
+	
+	std::cout << "Copying..." << std::endl;
+    str4.copy(txt4,11,9);
+	
+	//Safe to print because the char array was initialized with '\0' s.
+    std::cout << "txt4 : " << txt4 << std::endl;
+    */
+
+
     return 0;
 }
