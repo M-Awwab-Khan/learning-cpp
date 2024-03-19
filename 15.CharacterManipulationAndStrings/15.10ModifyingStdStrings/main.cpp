7e7bfeaa400100c682536b005c95ac13304960e0
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-19 16:13:42 +0500

    str.clear() method

---
+++ 15.CharacterManipulationAndStrings/15.10ModifyingStdStrings/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+
+int main(){
+
+     //Clear : Removes all characters from the string.
+     /*
+    std::string str1 {"The Lion Dad"};
+    
+    std::cout << "str1 is : " << str1 << std::endl;
+    std::cout << "str1 size : " << str1.size() << std::endl;
+    std::cout << "str1 capacity : " << str1.capacity() << std::endl;
+    
+    str1.clear();
+
+	std::cout << std::endl;
+    std::cout << "str1 is : " << str1 << std::endl;
+    std::cout << "str1 size : " << str1.size() << std::endl;
+    std::cout << "str1 capacity : " << str1.capacity() << std::endl;
+    */
+
+
+    return 0;
+}
